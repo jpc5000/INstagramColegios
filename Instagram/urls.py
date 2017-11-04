27edafc5722ff2a1b4b5728a5c28f54faeb00c24
Ático <atico@202C11E25168966.local>
2017-11-04 12:54:08 -0500

    tercer commit

--- Instagram/urls.py
+++ Instagram/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.signup, name= 'signup'),
+    url(r'^$', views.signup, name='signup'),
+    url(r'signin/$', auth_views.LoginView.as_view(template_name='signin.html',redirect_authenticated_user=True), name='signin'),
+    url(r'signout/$', auth_views.LogoutView.as_view(), name='signout'),
     url(r'^index/$', views.index, name='index'),
+    url(r'^profile/$', views.profile, name='profile'),
 ]
--- Instagram/views.py
+++ Instagram/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from Instagram.models import *
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 def signup(request):
+    if request.user.is_authenticated:
+        return redirect('index')
     if request.method == 'GET':
         return render(request, 'signup.html')
     else:
@@ -12,16 +15,18 @@ def signup(request):
         name = request.POST['nombre_completo']
         username = request.POST['usuario']
         password = request.POST['password']
-        print (email)
-        print (name)
-        print (username)
-        print (password)
-        usuarioDjango = User.objects.create_user(username = username, password = password, first_name = name)
+        usuarioDjango = User.objects.create_user(username = username, password = password, email=email, first_name = name)
         miUsuario = MiUsuario(usuario_django = usuarioDjango)
         usuarioDjango.save()
         miUsuario.save()
-        return redirect ('index')
+        return redirect ('signin')
 
+@login_required
 def index(request):
-    print (MiUsuario.objects.count())
     return render(request, 'index.html')
+
+@login_required
+def profile(request):
+    mi_usuario = MiUsuario.objects.get( pk = request.user.pk)
+    context = {'usuario_actual' : request.user}
+    return render(request, 'profile.html', context)
